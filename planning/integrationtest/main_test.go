53f63b3869f62bdc87022fb74e53ddb7ec5f7852
Karol Marcjan <karol.marcjan@gmail.com>
2019-08-21 01:00:57 +0200

    planning/integrationtest: Pick random port when starting server

--- planning/integrationtest/main_test.go
+++ planning/integrationtest/main_test.go
@@ -1,12 +1,16 @@
 package integrationtest
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
 	"time"
+
+	"github.com/szabba/assert"
 )
 
 func TestMain(m *testing.M) {
@@ -33,46 +37,63 @@ func buildApp() error {
 }
 
 type Server struct {
-	cmd *exec.Cmd
-	out []byte
-	err error
+	cmd  *exec.Cmd
+	port int
+	out  []byte
+	err  error
 }
 
 func (srv *Server) Start(t *testing.T) {
 	path, err := filepath.Abs(_Executable)
-	if err != nil {
-		t.Fatalf("failed to resolve %s: %s", _Executable, err)
-	}
+	assert.That(err == nil, t.Fatalf, "failed to resolve %s: %s", _Executable, err)
+
+	srv.port = srv.randomPort(t)
 
 	srv.cmd = exec.Command(path)
+	srv.cmd.Env = append(os.Environ(), fmt.Sprintf("PORT=%d", srv.port))
 	go srv.run(t)
+}
+
+func (srv *Server) Addr(t *testing.T) string {
+	assert.That(srv.port != 0, t.Fatalf, "server has not started yet, port is unknown")
+	return fmt.Sprintf("http://localhos:%d", srv.port)
+}
+
+func (srv *Server) randomPort(t *testing.T) int {
+	l, err := net.Listen("tcp", "localhost:0")
+	assert.That(err == nil, t.Fatalf, "cannot get random port: %s", err)
+
+	addr := l.Addr()
+	err = l.Close()
+	assert.That(err == nil, t.Fatalf, "cannot close socket listener: %s", err)
+
+	switch addr := addr.(type) {
+	case *net.TCPAddr:
+		return addr.Port
+	default:
+		t.Fatalf("cannot get port of %#v, unexpected type %T", addr, addr)
+		// unreachable
+		return 0
+	}
 }
 
 func (srv *Server) run(t *testing.T) {
 	srv.out, srv.err = srv.cmd.CombinedOutput()
-	if srv.err != nil {
-		t.Errorf("background server failure: %s", srv.err)
-	}
+	assert.That(srv.err == nil, t.Errorf, "background server failure: %s", srv.err)
 }
 
 func (srv *Server) Kill(t *testing.T) {
 	time.Sleep(100 * time.Millisecond) // scheduler hack
 
-	if srv.cmd == nil || srv.cmd.Process == nil {
-		t.Fatalf("cannot kill process that was not started")
-	}
+	assert.That(
+		srv.cmd != nil && srv.cmd.Process != nil,
+		t.Fatalf, "cannot kill process that was not started")
 
 	err := srv.cmd.Process.Kill()
-	if err != nil {
-		t.Logf("failed to kill %s (PID = %d): %s", _Executable, srv.cmd.Process.Pid, err)
-	}
+	assert.That(err == nil, t.Logf, "failed to kill %s (PID = %d): %s", _Executable, srv.cmd.Process.Pid, err)
 
 	err = srv.cmd.Wait()
-	if err != nil {
-		t.Logf("failed to wait for %s (PID = %d): %s", _Executable, srv.cmd.Process.Pid, err)
-	}
+	assert.That(err == nil, t.Logf, "failed to wait for %s (PID = %d): %s", _Executable, srv.cmd.Process.Pid, err)
 
-	if srv.cmd.ProcessState.ExitCode() != 0 {
-		t.Logf("%s", srv.out)
-	}
+	assert.That(srv.cmd.ProcessState.ExitCode() == 0, t.Logf, "%s", srv.out)
 }
